3752d49358bfecf5e6f31ffbdae74afb57e8df4c
Jen-Chun-Hsiang <jenchunhsiang@gmail>
2025-08-14 09:33:04 -0500

    more recordings

--- CompareModelPrediction8Responses_MovingBar_separateData.m
+++ CompareModelPrediction8Responses_MovingBar_separateData.m
@@ -2,12 +2,15 @@ clear all; close all; clc;
 %% specify the recording neurons
 load_data_folder ='\\storage1.ris.wustl.edu\kerschensteinerd\Active\Emily\PreyCaptureRGC\Results\MovingWhite\';
 stim_data_folder = '\\storage1.ris.wustl.edu\kerschensteinerd\Active\Emily\PreyCaptureRGC\Data\Stimulation\';
-recording_sets = {'b101424', 'c101424', 'd101424', 'e101424', 'a101624', 'b101624', 'd101624', 'e101624', 'd101924', 'e101924'};
+% recording_sets = {'b101424', 'c101424', 'd101424', 'e101424', 'a101624', 'b101624', 'd101624', 'e101624', 'd101924', 'e101924'};
+recording_sets = {'b103124',   'e103124', 'a110424',...
+             'c110424',                       'f110424', 'g110424',   'a110924', 'b110924', 'c110924',...
+             'a111224'};
 Fz = 100;
 implement_case_id = 6;
 num_recording = length(recording_sets);
 is_only_NL = 1;
-for ii = 3:num_recording
+for ii = 1:num_recording
     recording_name = recording_sets{ii};
     
     switch recording_name
@@ -87,6 +90,46 @@ for ii = 3:num_recording
             stim_wn_id = '002';
             stim_mb_id = '004';
             bar_type = 'OFF';
+         case 'b103124'
+            stim_wn_id = '002';
+            stim_mb_id = '003';
+            bar_type = 'ON';
+        case 'e103124'
+            stim_wn_id = '002';
+            stim_mb_id = '003';
+            bar_type = 'OFF';
+        case 'a110424'
+            stim_wn_id = '002';
+            stim_mb_id = '003';
+            bar_type = 'ON';
+        case 'c110424'
+            stim_wn_id = '001';
+            stim_mb_id = '002';
+            bar_type = 'ON';
+        case 'f110424'
+            stim_wn_id = '002';
+            stim_mb_id = '003';
+            bar_type = 'OFF';
+        case 'g110424'
+            stim_wn_id = '001';
+            stim_mb_id = '002';
+            bar_type = 'OFF';
+        case 'a110924'
+            stim_wn_id = '004';
+            stim_mb_id = '005';
+            bar_type = 'ON';
+        case 'b110924'
+            stim_wn_id = '008';
+            stim_mb_id = '009';
+            bar_type = 'OFF';
+        case 'c110924'
+            stim_wn_id = '003';
+            stim_mb_id = '005';
+            bar_type = 'OFF';
+        case 'a111224'
+            stim_wn_id = '003';
+            stim_mb_id = '004';
+            bar_type = 'ON';
     end
     response_name = recording_name;
     load_recording_name = recording_name;
