f829e76c197741d4cb2596c025bf4367af495b8a
Jen-Chun-Hsiang <jenchunhsiang@gmail>
2025-12-30 10:24:20 -0600

    Refactor draw_flatten_retina_disc_R.m for improved clarity and organization; enhance documentation and streamline parameter handling.

--- draw_flatten_retina_disc_R.m
+++ draw_flatten_retina_disc_R.m
@@ -1,7 +1,7 @@
 clear;  clc;
 % close all;
-% dataFile = 'C:\Users\jhsiang\Documents\R-retistruct\Test07\data.mat';
-dataFile = 'C:\Users\jhsiang\Documents\R-retistruct\kcng4\data.mat';
+dataFile = 'C:\Users\jhsiang\Documents\R-retistruct\Test07\data.mat';
+%dataFile = 'C:\Users\jhsiang\Documents\R-retistruct\kcng4\data.mat';
 
 
 
